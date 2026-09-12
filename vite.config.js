c354972dd3eaf979c76694bf4afe8a7f92bae9ec
dentex89 <ponchoelizondo@hotmail.com>
2026-09-12 13:20:07 -0600

    Configure GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { resolve } from "path";
 
 export default {
+  base: "/bootstrap5/",
   root: resolve(__dirname, "src"),
+
   build: {
     outDir: "../dist",
   },
+
   server: {
     port: 8080,
   },
-  // Optional: Silence Sass deprecation warnings. See note below.
+
+  // Optional: Silence Sass deprecation warnings
   css: {
     preprocessorOptions: {
       scss: {
